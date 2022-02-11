7bfb292eeb2ebba8d81ee62b49cb8e7d55392fae
Joseph Perez <jpere219@calpoly.edu>
2022-02-11 12:33:05 -0800

    new user format

--- bbbackend/models/user.js
+++ bbbackend/models/user.js
@@ -1,20 +1,35 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  job: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.length < 2) throw new Error("Invalid job.");
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.length < 2) throw new Error("Invalid job.");
+      },
+    },
+    profilepicture: {
+      type: String,
+      required: false,
+    },
+    friends: {
+      type: Object,
+      required: false,
+    },
+    lists: {
+      type: Object,
+      required: false,
     },
   },
-}, {collection : 'users_list'});
+  { collection: "users_list" }
+);
 
 const User = mongoose.model("User", UserSchema);
 
