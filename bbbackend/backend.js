cc99c2c1735aeeb78a6ed30776b8b9044de505ae
Mohamed Cheour <mcheour@calpoly.edu>
2022-02-28 11:00:30 -0800

    Commenting and Refactoring.

--- bbbackend/backend.js
+++ bbbackend/backend.js
@@ -9,10 +9,12 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-//in here
+
+/**
+ * Fetch all users when "/users" is added to the url.
+ * Separation of concerns: Calling the model component "user-services" 
+ * and not accessing the databse directly from here. 
+ */
 app.get("/users", async (req, res) => {
   const username = req.query["username"];
   const password = req.query["password"];
@@ -20,12 +22,17 @@ app.get("/users", async (req, res) => {
   try {
     const result = await userServices.getUsers(username, password);
     res.send({ users_list: result });
+  
   } catch (error) {
     console.log(error);
     res.status(500).send("An error ocurred in the server.");
   }
 });
 
+/**
+ * Adding a new user to the database once they sign up.
+ * To implement: check if user already exists.
+ */
 app.post("/users", async (req, res) => {
   let user = req.body;
 
@@ -33,43 +40,19 @@ app.post("/users", async (req, res) => {
     user.username,
     user.password
   );
+
   let savedUser = {};
 
   if (Object.keys(inDatabase).length === 0) {
     savedUser = await userServices.addUser(user);
-  }
-  if (savedUser || Object.keys(savedUser).length === 0) {
     res.status(201).send(savedUser);
-  } else res.status(500).end();
+  }
+
+  else {
+    res.status(500).end();
+  } 
 });
 
 app.listen(process.env.PORT || port, () => {
   console.log(`REST API is listening.:${port}`);
 });
-
-/* app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-}); */
-/*
-app.get("/users/:id", async (req, res) => {
-  const id = req.params["id"];
-  const result = await userServices.findUserById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
-  else {
-    res.send({ users_list: result });
-  }
-});
-*/
-app.delete("/users/:id", async (req, res) => {
-  const id = req.params["id"];
-  const result = await userServices.findUserById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
-  else {
-    userServices.removeUserById(result);
-    res.status(204).send("user removed");
-  }
-});
-
-// this one adds users
--- bbbackend/models/user-services.js
+++ bbbackend/models/user-services.js
@@ -1,109 +1,115 @@
 const mongoose = require("mongoose");
 const userModel = require("./user");
 const dotenv = require("dotenv");
-// prettier stylizing import
-const {
-  compareByGeneratedPositionsDeflated,
-} = require("prettier/parser-postcss");
 
-// sets up the cluster for the MongoDB interaction
 
+const {compareByGeneratedPositionsDeflated,} = require("prettier/parser-postcss");
 dotenv.config();
-console.log(
-  "mongodb+srv://" +
-    process.env.MONGO_USER +
-    ":" +
-    process.env.MONGO_PWD +
-    "@cluster0.g187p.mongodb.net/" +
-    process.env.MONGO_DB +
-    "?retryWrites=true&w=majority"
-);
-mongoose
-  .connect(
-    "mongodb+srv://" +
-      process.env.MONGO_USER +
-      ":" +
-      process.env.MONGO_PWD +
-      "@cluster0.g187p.mongodb.net/" +
-      process.env.MONGO_DB +
-      "?retryWrites=true&w=majority",
-    // "mongodb://localhost:27017/users",
+
+mongoose.connect("mongodb+srv://" + process.env.MONGO_USER + ":" +
+    process.env.MONGO_PWD + "@cluster0.g187p.mongodb.net/" +
+      process.env.MONGO_DB + "?retryWrites=true&w=majority",
     {
-      useNewUrlParser: true, //useFindAndModify: false,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error)
+);
 
-// function to get users from the MongoDB:
-// checks if the username and password passed to the function are valid
-// 1) If they are not, all users are returned
-// 2) If they are, findUserByNameAndPassword is called to find the correct user based on the username and password passed in
 
+/**
+ * Fetch all users from the database if credentials (username, password) are not valid.
+ * Fetch user if credentials are valid. 
+ * @param {*} username 
+ * @param {*} password 
+ * @returns 
+ */
 async function getUsers(username, password) {
   let result;
 
   if (username === undefined && password == undefined) {
     result = await userModel.find();
-  } else if (username && password) {
+  } 
+  
+  else if (username && password) {
     result = await findUserByNameAndPassword(username, password);
   }
 
   return result;
 }
 
-// function to add a new user to a database based on the username and password that was input
-// sets defaults like the profile picture using the setDefaults function
 
+/**
+ * Add a new user to a database based on the username and password provided by the user.
+ * Set default user attributes (profile picture, friends' list etc...) for every new user.
+ * @param {*} user 
+ * @returns new user 
+ */
 async function addUser(user) {
   try {
+    
     const userToAdd = new userModel(user);
     setDefaults(userToAdd);
     const savedUser = await userToAdd.save();
+    
     return savedUser;
+  
   } catch (error) {
     return false;
   }
 }
 
 
-// called in the addUser function, gives the new user an empty list for friends, groups, inventory, history
-
+/**
+ * Give a new user an empty list for each of there attributes: friends, groups, inventory, history.
+ * History object has to be built out to include more functionality.
+ * @param {*} userToAdd 
+ */
 function setDefaults(userToAdd) {
   userToAdd.friends = { friendList: [], friendCount: 0 };
   userToAdd.groups = { groupList: [], groupCount: 0 };
   userToAdd.inventory = { itemList: [], itemCount: 0 };
-  // history object has to be built out to include more functionality
   userToAdd.history = { purchaseList: [], purchaseCount: 0 };
   userToAdd.profilepicture =
     "https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg";
 }
 
-// pulls a user from the database based on their username and password
 
+/**
+ * Fetch user from the database given its username and password.
+ * @param {*} username 
+ * @param {*} password 
+ * @returns user
+ */
 async function findUserByNameAndPassword(username, password) {
   return await userModel.find({ username: username, password: password });
 }
 
-// FUNCTIONS NOT USED IN ACTIVE CODE(leftover), USEFUL FOR LATER
+
+exports.getUsers = getUsers;
+exports.addUser = addUser;
+exports.findUserByNameAndPassword = findUserByNameAndPassword;
+
+/*FUNCTIONS NOT USED IN ACTIVE CODE(leftover), USEFUL FOR LATER
 
 // deletes a user from the database based on their id, no functionality assosciated with this yet, but eventually we should be able to delete accounts
-/*
+
 async function removeUserById(id) {
   let result;
   result = await userModel.findByIdAndDelete(id);
   return result;
 }
-*/
+
 // pulls a user from the database based on their username, no functionality assosciated with this yet, but eventually we may need this lookup
-/*
+
 async function findUserByName(name) {
   return await userModel.find({ username: name });
 }
-*/
+
 // pulls a user from the database based on the _id, no functionality assosciated with this yet, but eventually we may need this lookup
 
-/*
+
 async function findUserById(_id) {
   try {
     return await userModel.findById(_id);
@@ -112,13 +118,10 @@ async function findUserById(_id) {
     return undefined;
   }
 }
-*/
+
 
 // export statements
-
-exports.getUsers = getUsers;
-exports.addUser = addUser;
-exports.findUserByNameAndPassword = findUserByNameAndPassword;
-//exports.findUserById = findUserById;
-//exports.findUserByName = findUserByName;
-//exports.removeUserById = removeUserById;
+exports.findUserById = findUserById;
+exports.findUserByName = findUserByName;
+exports.removeUserById = removeUserById;
+*/
