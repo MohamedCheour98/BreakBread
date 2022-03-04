b95a37f5202beb9d790f26b9db4000808d8c59a9
Hannah Tobiason <hanbtob@gmail.com>
2022-03-04 13:52:00 -0800

    jest setup, npm install in the backend. First test passes

--- bbbackend/functions.test.js
+++ bbbackend/functions.test.js
@@ -1,14 +1,13 @@
 const userServices = require("./models/user-services");
 const mongoose = require("mongoose");
 const ObjectId = require("mongoose").Types.ObjectId;
-const { test } = require("prettier");
 
-test("test db query user Joe", async () => {
-  let result = await userServices.findUserByName("joe");
+test("test db query user joe", async () => {
+  let result = await userServices.findUserByNameAndPassword("joe", "joejoe");
   console.log(result);
   console.log(result[0]);
-  console.log(result[0].name);
+  console.log(result[0].username);
 
-  expect(result[0].name).toBe("joe");
+  expect(result[0].username).toBe("joe");
   expect(result[0].password).toBe("joejoe");
 });
