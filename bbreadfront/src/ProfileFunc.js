6611a56af13e25db227eae7816e58263c7501b38
Mohamed Cheour <mcheour@calpoly.edu>
2022-03-05 22:31:09 -0800

    Update ProfileFunc.js

--- bbreadfront/src/ProfileFunc.js
+++ bbreadfront/src/ProfileFunc.js
@@ -11,16 +11,17 @@ function ProfileFunc() {
   let history = useHistory();
   let location = useLocation();
   let currentUser = location.state.user.username;
-
   const [users, setCharacters] = useState([]);
-function removeOneCharacter (index) {
-  const deleted = users[index];
-  const updated = users.filter((user, i) => {
-      return i !== index
+  
+  function removeOneCharacter (index) {
+    const deleted = users[index];
+    const updated = users.filter((user, i) => {
+        return i !== index
     });
     deleteBackend(deleted['_id']);
     setCharacters(updated);
   }
+
   async function deleteBackend(_id) {
     try {
       const response = await axios.delete('http://localhost:5000/users/' + _id);
@@ -33,12 +34,13 @@ function removeOneCharacter (index) {
       return false;
     }
   }
+
   function updateList(person) { 
     makeGetCall(person).then( result => {
     /*if (result && result.status === 201)
        setCharacters([...characters, result.data] );*/
     });
- }
+  }
   async function fetchAll(){
     try {
        const response = await axios.get('http://localhost:5000/users');
@@ -49,45 +51,49 @@ function removeOneCharacter (index) {
        console.log(error); 
        return false;         
     }
- }
- async function makeGetCall(person){
-  try {
-     const response = await axios.get('http://localhost:5000/users', person);
-     return response;
   }
-  catch (error) {
-     console.log(error);
-     return false;
+
+  async function makeGetCall(person){
+    try {
+      const response = await axios.get('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
   }
-}
-async function addFriend(){
-  //dont know know what to return from put just because there is only a status that is sent back
-  try {
-    const response = await axios.put('http://localhost:5000/users', {user: currentUser , friend: "come"});
-    
-    return response;
- }
- catch (error) {
-    console.log(error);
-    return false;
- }
-}
-function deleteFriend(){
-  console.log("deletefriend");
-}
- useEffect(() => {
-  fetchAll().then( result => {
-     if (result)
-        setCharacters(result);
-   });
-}, [] );
-    return (
-      <div className="container">
-        <FriendTable user={location.state.user}/>
-        <InventoryTable user={location.state.user}/>
-        <ProfileForm addFriend= {addFriend} deleteFriend={deleteFriend} />
-      </div>
-    );  
+
+  async function addFriend(){
+    //dont know know what to return from put just because there is only a status that is sent back
+    try {
+      const response = await axios.put('http://localhost:5000/users', {user: currentUser , friend: "come"});
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function deleteFriend(){
+    console.log("deletefriend");
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+    if (result)
+      setCharacters(result);
+    });
+  }, [] );
+  
+  return (
+    <div className="container">
+      <FriendTable user={location.state.user}/>
+      <InventoryTable user={location.state.user}/>
+      <ProfileForm addFriend= {addFriend} deleteFriend={deleteFriend} />
+    </div>
+  );  
 }
 
 export default ProfileFunc;
