40ef770938146ef697089a34857b6de0796edcd9
Erik McCutchen <erikmccutchen@gmail.com>
2022-03-07 11:57:58 -0800

    made table render quantity, price, user, and item, started working on trying to route back to profile, but decided to make it a separate branch

--- bbreadfront/src/GroceryRun.js
+++ bbreadfront/src/GroceryRun.js
@@ -11,23 +11,26 @@ function GroceryRun(props){
     const [person, setPerson] = useState({
         item: "",
         price: "",
+        quantity: "",
         user: "",
     })
 
     function handleChange(event) {
         const { name, value } = event.target; /* added inventory*/
         if (name === "price")
-          setPerson({ item: person["item"], price: value, user: person["user"]});
+          setPerson({ item: person["item"], price: value, quantity: person["quantity"], user: person["user"]});
         else if (name === "item")
-            setPerson({ item: value, price: person["price"], user: person["user"]});
-        else(setPerson({ item: person["item"], price: person["price"], user: value}))
+            setPerson({ item: value, price: person["price"], quantity: person["quantity"], user: person["user"]});
+        else if (name === "quantity")
+          setPerson({ item: person["item"], price: person["price"], quantity: value, user: person["user"]})
+        else(setPerson({ item: person["item"], price: person["price"], quantity: person["quantity"], user: value}))
 
       }
 
     async function submitForm() {
       let newPerson = person
       inventory.push(newPerson)
-      setPerson({ item: "", price: "", user: ""});
+      setPerson({ item: "", price: "", quantity: "", user: ""});
       }
 
       async function makePatchCall(person) {
@@ -65,6 +68,13 @@ function GroceryRun(props){
           value={person.price}
           onChange={handleChange}
         />
+        <label htmlFor="quantity">Quantity</label>
+        <input
+          type="text"
+          name="quantity"
+          value={person.quantity}
+          onChange={handleChange}
+        />
         <label htmlFor="user">User</label>
         <input
           type="text"
@@ -72,6 +82,7 @@ function GroceryRun(props){
           value={person.user}
           onChange={handleChange}
         />
+        
       </div>
 
       <input type="button" value="add item" onClick={submitForm} />
--- bbreadfront/src/ProfileForm.js
+++ bbreadfront/src/ProfileForm.js
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 
 function ProfileForm(props) {
-  
-  console.log("we in here?");
-  console.log(props);
   const [person, setPerson] = useState({
     name: "",
     password: ""
   });
+  //const [redirect, setRedirect] = React.useState(false);
+  //const [userData, setUserData] = useState({});
 
+
+  function submitForm() {
+    props.handleSubmit(person);
+    //setRedirect(true);
+    setPerson({ username: "", password: "" }); /*  id: ''  this was added*/
+  }
+  /*
   return (
     <container>
       <div className = "grocery">
-        
-      <Link to = "/grocery" className = "button"> New run </Link>
-    
+      <input type="button" value="New Run" onClick={submitForm} />
+      {redirect  ? (<div>
+        <Redirect to={{pathname: "/profile", state: {user: userData}}}  />
+      </div> 
+      ): null} 
+
       </div>
     </container>
+
     
   );
-
+  */
+  return (
+    <container>
+      <div className = "grocery">
+      <Link to = "/grocery" className = "button"> New run </Link>    
+      </div>
+    </container>
+  );
 }
 
 
